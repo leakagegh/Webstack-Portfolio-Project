23a5de86df9c0797594a1a4acc8eb3e862ae7853
leakagegh <thehealer184@gmail.com>
2024-06-26 12:42:31 +0000

    Cont

---
+++ homemadegrub_react/src/pages/form.js
@@ -0,0 +1,18 @@
+<div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <div class="bg-white p-8 rounded shadow-md w-96">
+            <h2 class="text-2xl font-semibold mb-4">Login</h2>
+            <form>
+                <div class="mb-4">
+                    <label for="email" class="block text-gray-600">Email</label>
+                    <input type="email" id="email" name="email" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500" placeholder="Enter your Email" required />
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-600">Password</label>
+                    <input type="password" id="password" name="password" class="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500" placeholder="Enter your password" required />
+                </div>
+                <div class="mb-4">
+                    <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
---
+++ homemadegrub_react/src/pages/index.js
@@ -0,0 +1,20 @@
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import MainPage from "./MainPage";
+import OrderPage from "./OrderPage";
+import SignUpClientPage from "./SignUpClientPage";
+import SignUpChefPage from "./SignUpChefPage";
+import SignUpDeliveryPage from "./SignUpDeliveryPage";
+import CartPage from "./CartPage";
+
+
+export {
+    LoginPage,
+    MainPage,
+    OrderPage,
+    SignUpClientPage,
+    SignUpChefPage,
+    SignUpDeliveryPage,
+    SignUpPage,
+    CartPage
+};
